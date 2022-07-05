d83c4a1c47630f2d3995d663f6bdda1416b35c9e
irakli9909 <razora.irakli@gmail.com>
2022-07-05 14:23:58 +0400

    add home page

---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Home.css";
+
+function Home() {
+  return (
+    <div>
+      <button className='button-items'>Start</button>
+    </div>
+  )
+}
+
+export default Home
