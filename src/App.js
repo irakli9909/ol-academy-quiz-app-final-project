1f25df1edc8792f1967dd695b72b6cae501dabf1
irakli9909 <razora.irakli@gmail.com>
2022-07-11 17:09:22 +0400

    remove link tag

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route,} from 'react-router-dom';
 import Home from './components/Home';
 import Quiz from './components/Quiz';
 import History from "./components/History/History";
